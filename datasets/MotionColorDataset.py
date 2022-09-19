909ff6d20e09d478543aa381a34c65e84ffa50f3
miles <mjm382@bu.edu>
2022-09-19 14:10:42 -0700

    working color/motion context classifier 88% accuracy in 100 epochs

--- datasets/MotionColorDataset.py
+++ datasets/MotionColorDataset.py
@@ -2,12 +2,15 @@ from torch.utils.data import Dataset
 import numpy as np
 
 
+def one_hot(choice):
+    return np.array([1, 0, 0], dtype=np.float32) if choice == -1 else np.array([0, 0, 1], dtype=np.float32)
+
+
 class MotionColorDataset(Dataset):
     def __init__(self, num_samples, seq_length):
         self.num_samples = num_samples
         self.seq_length = seq_length
 
-        # TODO just two random vars for now
         ### distribution variables
         ## color
         sigma_c = 0.5
@@ -25,21 +28,29 @@ class MotionColorDataset(Dataset):
         self.motion_gen_right = np.float32(np.random.normal(mu_right, sigma_m, seq_length))
         self.motion_gen_left = np.float32(np.random.normal(mu_left, sigma_m, seq_length))
 
-    # returns seq_length samples from either the red or green distribution and the left/right distribution
+    # returns seq_length samples from either the red or green distribution and the left or right distribution
     def __getitem__(self, index):
+        # green or red (color)
         coin_flip = np.random.binomial(1, 0.5)
         color_gen, color_label = (self.color_gen_g, -1) if coin_flip == 0 else (self.color_gen_r, 1)
 
+        # left or right (direction)
         coin_flip = np.random.binomial(1, 0.5)
         motion_gen, motion_label = (self.motion_gen_right, -1) if coin_flip == 0 else (self.motion_gen_left, 1)
+
+        # color or motion (context)
+        context = np.random.randint(0, 2)
+
         out = []
-        for i in range (self.seq_length):
-            out.append([color_gen[i], motion_gen[i], np.float32(0)])
+        for i in range(self.seq_length):
+            out.append([color_gen[i], motion_gen[i], np.float32(context)])
+
+        label = one_hot(color_label if context else motion_label)
 
         # sequence, label
-        # [seq_length x [3]], [2]
-        # TODO in this config, model is learning both color and motion (both labels are provided)
-        return out, [np.float32(color_label), np.float32(motion_label)]
+        # [seq_length x [color, motion, context]], [one-hot encoding of the three classes (-1, 0, 1)]
+        # color if context == 1 else motion
+        return out, label
 
     def __len__(self):
         return self.num_samples
--- nn_rnn_mante.py
+++ nn_rnn_mante.py
@@ -1,20 +1,24 @@
+import time
+
 import torch
 import torch.nn as nn
 import numpy as np
 
 # Device configuration
+from torch.utils.data import DataLoader
+
 from datasets.MotionColorDataset import MotionColorDataset
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 # Hyperparameters
-num_epochs = 1000
+num_epochs = 100
 learning_rate = 0.0001
 hidden_size = 128
 num_layers = 1
 batch_size = 100
 input_size = 3  # (motion (float), color (float), context (bool/int))
-output_size = 2  # (motion l/r, color r/g (bool/ints))
+output_size = 3  # (-1, 0, 1)
 
 # Dataset params
 train_dataset = MotionColorDataset(100, 10)
@@ -27,12 +31,14 @@ class RNN(nn.Module):
         self.num_layers = num_layers
         self.hidden_size = hidden_size
         self.rnn = nn.RNN(input_size, hidden_size, num_layers, batch_first=True)
-        self.fc = nn.Linear(hidden_size, output_size)
+        self.fc1 = nn.Linear(hidden_size, 64)
+        self.fc2 = nn.Linear(64, output_size)
 
     # if x is a batch, no need to pass hidden state beyond initial h0
     def forward(self, x, hidden):
         out, hidden_i = self.rnn(x, hidden)
-        out = self.fc(out)
+        out = self.fc1(out)
+        out = self.fc2(out)
         return out, hidden_i
 
     def init_hidden(self):
@@ -48,20 +54,30 @@ optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
 # training
 print('training model')
 n_total_steps = 1000
+time_start = time.perf_counter()
 for epoch in range(num_epochs):
     for i, (dots, label) in enumerate(train_dataset):
         hidden = model.init_hidden()
         for dot in dots:
             output, hidden = model(torch.from_numpy(np.array([dot])), hidden)
         optimizer.zero_grad()
-        loss = criterion(output.view(-1), torch.from_numpy(np.array(label)))
-        # print(loss)
-        if (i + 1) % 100 == 0:
-            print(f'Epoch [{epoch + 1}/{num_epochs}], Step [{i + 1}/{n_total_steps}], Loss: {loss.item()*1000:.6f}')
+        loss = criterion(output.view(-1), torch.from_numpy(label))
         loss.backward()
         optimizer.step()
         if i >= n_total_steps:
             break
+    if (epoch + 1) % 10 == 0:
+        print(f'Epoch [{epoch + 1}/{num_epochs}], Step [{i}/{n_total_steps}], Loss: {loss.item() * 1000:.6f}')
+        print(f'Time elapsed: {time.perf_counter() - time_start:0.2f} seconds')
+
+
+# from model output, return -1, 0 or 1
+def translate_output(x):
+    classes = [-1, 0, 1]
+    prob = nn.functional.softmax(x[-1], dim=0).data
+    choice = classes[torch.max(prob, dim=0)[1].item()]
+    return choice
+
 
 FILE = "model.pth"
 torch.save(model.state_dict(), FILE)
@@ -75,9 +91,8 @@ with torch.no_grad():
         hidden = model.init_hidden()
         for dot in dots:
             output, hidden = model(torch.from_numpy(np.array([dot])), hidden)
-        if torch.equal(output.view(-1), torch.from_numpy(np.array(label))):
+        if translate_output(output) == translate_output(torch.from_numpy(label)):
             n_correct += 1
-            print(n_correct)
         loss += criterion(output.view(-1), torch.from_numpy(np.array(label)))
         if i == n_samples:
             break
